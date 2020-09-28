998287b41fe5fed0b1e7210389d353895c750561
McKlobasa <kl.lampret@gmail.com>
2020-09-28 13:35:43 +0100

    paypal povratna informacija

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const stripe = require('stripe')('sk_test_51HTk05KfFZQiWrPrEvFmmvjYs27EeySW2x4YyX2fBvBa69UbH6Mtw1WRtqBwQ33IafDgRx3HJ4bn0SkGL0dyg8Wl001pt7oSOn');
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'))
@@ -32,8 +33,16 @@ const addPaymentToDataBase = ( paymentInfo ) => {
 const formateStripePayerInfo = (stripeInfo) => {
   return new PaymentInfo('ivan', 'anon', 'ivan.anon@gmail.com', '4:20:690__11_9_2020', 'one_game')
 }
-const formatePayPalPayerInfo = (payPalInfo) => {
-  return new PaymentInfo('ivan', 'anon', 'ivan.anon@gmail.com', '4:20:690__11_9_2020', 'one_game')
+const formatPayPalPayerInfo = (payPalInfo) => {
+  console.log('uuuu senpai ur server got paypaaaaal infoooo')
+  console.log(payPalInfo)
+  return new PaymentInfo(
+    payPalInfo.payer.name.given_name, 
+    payPalInfo.payer.name.surname, 
+    payPalInfo.payer.email.email_address, 
+    payPalInfo.update_time, 
+    'paypal_one_stream'
+  )
 }
 
 const doOnPayment = ( paymentInfo ) => {
@@ -58,21 +67,59 @@ app.post("/create-checkout-session", async (req, res) => {
       },
     ],
     mode: "payment",
-    success_url: "https://example.com/success",
+    success_url: "http://localhost:4242/success",
     cancel_url: "https://example.com/cancel",
   });
 
   res.json({ id: session.id });
 });
+app.post('/onPayPalPayment', bodyParser.raw({type: 'application/json'}), (req, res) => {
+  console.log('startData =', req.body)
+  doOnPayment(formatPayPalPayerInfo(req.body))
+  res.sendStatus(200)
+})
 
 app.get('/', function(req, res) {
   res.sendFile(`${__dirname}/views/main.html`);
 });
+app.get('/success', function(req, res) {
+  res.sendFile(`${__dirname}/views/success.html`);
+});
 app.get('/style.css', function(req, res) {
   res.sendFile(`${__dirname}/style.css`);
 });
-app.get('/addUser/', function (req, res) {
-  doOnPayment()
-})
+
+
+
+/*Stripe on payment test*/
+
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+  let event;
+
+  try {
+    event = JSON.parse(request.body);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log('PaymentIntent was successful!');
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      console.log('PaymentMethod was attached to a Customer!');
+      break;
+    // ... handle other event types
+    default:
+      // Unexpected event type
+      return response.status(400).end();
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.json({received: true});
+});
 
 app.listen(4242, () => console.log(`Listening on port ${4242}!`));
